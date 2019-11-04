f6d96abf7ef7c2f2283e5a56c8e81bec7df0efd7
Orkhan Ahmadov <ahmadov90@gmail.com>
2019-11-04 17:17:25 +0100

    WIP

--- config/config.php
+++ config/config.php
@@ -2,6 +2,15 @@
 
 return [
 
+    /*
+    |--------------------------------------------------------------------------
+    | Comments table name
+    |--------------------------------------------------------------------------
+    |
+    | Define with table name should be used for comments table.
+    |
+    */
+
     'table_name' => 'comments',
 
 ];
